87752e901f1bc07898ecfb2844fc2fcf83199b24
Adrianmrc94 <adrianmrc94@gmail.com>
2025-01-07 14:16:59 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const sujetos = ["Mi perro", "Mi novia", "Un mendigo", "Un alien"];
+const acciones = ["me robó", "compró", "mordió", "puso pegamento a"];
+const objetos = ["mis deberes", "mi portatil", "mi coche", "mis zapatillas"];
+const lugares = [
+  "llegando al sitio",
+  "al salir de casa",
+  "al salir del trabajo",
+  "en casa de mi abuela"
+];
+
+function generarExcusa() {
+  const sujeto = sujetos[Math.floor(Math.random() * sujetos.length)];
+  const accion = acciones[Math.floor(Math.random() * acciones.length)];
+  const objeto = objetos[Math.floor(Math.random() * objetos.length)];
+  const lugar = lugares[Math.floor(Math.random() * lugares.length)];
+  return `${sujeto} ${accion} ${objeto} ${lugar}`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excusa").textContent = generarExcusa();
 };
